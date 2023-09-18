08b692194c518ae5423618ab5108f6a454800e46
edwards955 <getignorantfriday@gmail.com>
2023-09-18 13:36:54 -0400

    Add clean_phone method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,16 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def clean_phone(phone)
+  if phone.length < 10 || (phone.length == 11 && phone[0] != 1) || phone.length > 11
+    phone.rjust(10, '0')
+  elsif phone.length == 11 && phone[0] == 1
+    phone[1, phone.length]
+  else
+    phone
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -45,8 +55,11 @@ erb_template = ERB.new template_letter
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+  phone = row[:homephone]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
+
+
 
   form_letter = erb_template.result(binding)
 
